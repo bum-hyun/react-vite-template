170312a49b51cde7eb0d7a340dcb0e0cfc5b85b2
bum-hyun <ru_bryunak@naver.com>
2025-04-11 10:42:37 +0900

    feature: supabase 관련 파일 추가

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,9 +1,8 @@
-import { createBrowserClient } from '@supabase/ssr';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const createClient = () => createBrowserClient(supabaseUrl, supabaseKey);
-const browserClient = createClient();
+const supabase =  createClient(supabaseUrl, supabaseKey);
 
-export default browserClient;
+export default supabase;
